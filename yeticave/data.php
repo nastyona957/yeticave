6770beeb54c305914a1f76149e3e471dcefd90c0
Anastasia Shtrezer <94467334+nastyona957@users.noreply.github.com>
2023-12-22 15:27:38 +0300

    Add files via upload

---
+++ yeticave/data.php
@@ -0,0 +1,56 @@
+<?php
+
+$categories = [
+    "boards" => "Доски и лыжи",
+    "attachment" => "Крепления",
+    "boots" => "Ботинки",
+    "clothing" => "Одежда",
+    "tools" => "Инструменты",
+    "other" => "Разное"
+  ];
+
+$goods = [
+
+    [
+        "title" => "2014 Rossignol District Snowboard",
+        "category" => $categories["boards"],
+        "price" => 10999,
+        "image" => "img/lot-1.jpg",
+        "expiration" => "2023-12-19"
+     ],
+     [
+        "title" => "DC Ply Mens 2016/2017 Snowboard",
+        "category" => $categories["boards"],
+        "price" => 159999,
+        "image" => "img/lot-2.jpg",
+        "expiration" => "2023-12-18"
+     ],
+     [
+        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
+        "category" => $categories["attachment"],
+        "price" => 8000,
+        "image" => "img/lot-3.jpg",
+        "expiration" => "2023-12-17"
+     ],
+     [
+        "title" => "Ботинки для сноуборда DC Mutiny Charocal",
+        "category" => $categories["boots"],
+        "price" => 	10999,
+        "image" => "img/lot-4.jpg",
+        "expiration" => "2023-12-16"
+     ],
+     [
+        "title" => "Куртка для сноуборда DC Mutiny Charocal",
+        "category" => $categories["clothing"],
+        "price" => 7500,
+        "image" => "img/lot-5.jpg",
+        "expiration" => "2023-12-15"
+     ],
+     [
+        "title" => "Маска Oakley Canopy",
+        "category" => $categories["other"],
+        "price" => 5400,
+        "image" => "img/lot-6.jpg",
+        "expiration" => "2023-12-20"
+     ],
+];
