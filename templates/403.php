838316ed3344f71709f551b92918baf0ed753b98
nastyona957 <columbia957@gmail.com>
2024-02-03 20:13:29 +0300

    +add 403

---
+++ templates/403.php
@@ -0,0 +1,10 @@
+<main>
+    <?= $header; ?>
+    <section class="lot-item container">
+            <h2>403 Forbidden</h2>
+            <p>У вас нет прав доступа к содержимому этой страницы</p>
+    </section>
+  </main>
+
+</div>
+
